317d19993571b2ec0e8cbdd413fd188632662ce9
Niemann <Daniel.Niemann@eventim.de>
2019-01-18 19:37:49 +0100

    delete unused prop from button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button onClick={props.onClick} className="form-button">
+        <button className="form-button">
             {props.message}
         </button>
     );
